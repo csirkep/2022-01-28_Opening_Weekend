5c56a5ef49dc1db5e7a7b0f792c27f1ff434a57f
csirkep <csirkep.12d@acsjszki.hu>
2022-01-28 09:35:17 +0100

    2.

--- 2022-01-28_Opening_Weekend/OpeningWeekend.cs
+++ 2022-01-28_Opening_Weekend/OpeningWeekend.cs
@@ -11,6 +11,12 @@ namespace _2022_01_28_Opening_Weekend
     {
         static void Main(string[] args)
         {
+            List<Film> filmek = new List<Film>();
+            foreach (var sor in File.ReadAllLines("nyitohetvege.txt").Skip(1))
+            {
+                filmek.Add(new Film(sor));
+            }
+
 
             Console.ReadKey();
         }
