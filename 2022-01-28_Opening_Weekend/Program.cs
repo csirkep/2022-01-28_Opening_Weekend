0c2b3b7bd58dd067a10e950ab75bc117d73691a6
csirkep <csirkep.12d@acsjszki.hu>
2022-01-28 09:12:30 +0100

    Add project files.

---
+++ 2022-01-28_Opening_Weekend/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_01_28_Opening_Weekend
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
